73f33558592c52e268de953c86a1ac6d31d6b5c7
Peter Harrison <peter.harrison@helicron.net>
2016-02-08 10:21:43 +0000

    added first draft of maze api

--- maze.c
+++ maze.c
@@ -1,7 +1,14 @@
 /* 
- * File:   %<%NAME%>%.%<%EXTENSION%>%
- * Author: %<%USER%>%
+ * File:   maze.c
+ * Author: peterharrison
  *
- * Created on %<%DATE%>%, %<%TIME%>%
+ * Created on 08 February 2016, 00:09
  */
+
+#include <stdint.h>
+#include "maze.h"
+
+walls_t walls[MAZE_WIDTH * MAZE_WIDTH];
+cost_t cost[MAZE_WIDTH * MAZE_WIDTH];
+direction_t direction[MAZE_WIDTH * MAZE_WIDTH];
 
