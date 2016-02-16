c7bfb9f59f22ca4882cde844a8cb0cf2c6476fd5
Peter Harrison <peter.harrison@helicron.net>
2016-02-16 15:04:43 +0000

    add empty pathfinder source files

---
+++ mazepathfinder.c
@@ -0,0 +1,11 @@
+/* 
+ * File:   mazepathfinder.c
+ * Author: peterharrison
+ *
+ * Created on 16 February 2016, 14:55
+ */
+
+#include "maze.h"
+#include "mazepathfinder.h"
+
+
---
+++ mazepathfinder.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   mazepathfinder.h
+ * Author: peterharrison
+ *
+ * Created on 16 February 2016, 14:55
+ */
+
+#ifndef MAZEPATHFINDER_H
+#define MAZEPATHFINDER_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* MAZEPATHFINDER_H */
+
