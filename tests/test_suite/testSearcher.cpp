1a454dee812a0c5f833cf3d4d848c7460112f9d0
Peter Harrison <peter.harrison@helicron.net>
2017-06-27 21:42:16 +0100

    update MazeSearcher tests

--- tests/test_suite/testSearcher.cpp
+++ tests/test_suite/testSearcher.cpp
@@ -11,8 +11,8 @@ class SearcherTest : public ::testing::Test {
   virtual void SetUp() {
     maze = new Maze(16);
     maze->copyMazeFromFileData(japan2007, 256);
-
-    barney = new MazeSearcher;
+    searcher = new MazeSearcher;
+    searcher->setRealMaze(maze);
 
 //    MazePrinter::printPlain(&testMaze);
 //    char testMazeName[] = "../mazefiles/minos03f.maz";
@@ -30,54 +30,83 @@ class SearcherTest : public ::testing::Test {
   }
 
   virtual void TearDown() {
-    delete barney;
+    delete searcher;
     delete maze;
   }
 
  protected:
-  MazeSearcher *barney;
+  MazeSearcher *searcher;
   Maze *maze;
 };
 
 TEST_F (SearcherTest, Constructor) {
-  EXPECT_EQ(0, barney->location());
-  EXPECT_EQ(NORTH, barney->heading());
-  EXPECT_EQ(NULL, barney->realMaze());
+  EXPECT_EQ(0, searcher->location());
+  EXPECT_EQ(NORTH, searcher->heading());
+  EXPECT_NE(nullptr, searcher->realMaze());
+  searcher->map()->flood(0x77);
+  EXPECT_EQ(14,searcher->map()->cost(0x00));
 }
 
 TEST_F (SearcherTest, MouseSetPosition_PositionAsSet) {
-  barney->setLocation(0x99);
-  EXPECT_EQ(0x99, barney->location());
+  searcher->setLocation(0x99);
+  EXPECT_EQ(0x99, searcher->location());
 }
 
 TEST_F (SearcherTest, MouseSetHeading_HeadingAsSet) {
-  barney->setHeading(EAST);
-  EXPECT_EQ(EAST, barney->heading());
+  searcher->setHeading(EAST);
+  EXPECT_EQ(EAST, searcher->heading());
 }
 
 
 TEST_F (SearcherTest, moveWithMaze_ChangeLocationOnly) {
-  barney->setMazeWalls(japan2007, 256);
-  barney->setLocation(0x00);
-  barney->setHeading(NORTH);
-  barney->move();
-  EXPECT_EQ(0x01, barney->location());
-  EXPECT_EQ(NORTH,barney->heading());
+  searcher->move();
+  EXPECT_EQ(0x01, searcher->location());
+  EXPECT_EQ(NORTH,searcher->heading());
 }
 
+
+
+TEST_F (SearcherTest, Turning_ChangeHeadingOnly) {
+  searcher->setLocation(0x44);
+  EXPECT_EQ(NORTH,searcher->heading());
+  searcher->turnRight();
+  EXPECT_EQ(EAST,searcher->heading());
+  searcher->turnRight();
+  EXPECT_EQ(SOUTH,searcher->heading());
+  searcher->turnRight();
+  EXPECT_EQ(WEST,searcher->heading());
+  searcher->turnRight();
+  EXPECT_EQ(NORTH,searcher->heading());
+
+  searcher->turnLeft();
+  EXPECT_EQ(WEST,searcher->heading());
+  searcher->turnLeft();
+  EXPECT_EQ(SOUTH,searcher->heading());
+  searcher->turnLeft();
+  EXPECT_EQ(EAST,searcher->heading());
+  searcher->turnLeft();
+  EXPECT_EQ(NORTH,searcher->heading());
+
+  searcher->turnAround();
+  EXPECT_EQ(SOUTH,searcher->heading());
+
+  EXPECT_EQ(0x44,searcher->location());
+}
+
+
+
 TEST_F (SearcherTest, moveWithMaze_smallCircuitReturnToStart) {
-  barney->setMazeWalls(japan2007, 256);
-  barney->setLocation(0x39);
-  barney->move();
-  barney->setHeading(EAST);
-  barney->move();
-  barney->setHeading(SOUTH);
-  barney->move();
-  barney->setHeading(WEST);
-  barney->move();
-  barney->setHeading(NORTH);
-  EXPECT_EQ(0x39, barney->location());
-  EXPECT_EQ(NORTH,barney->heading());
+  searcher->setLocation(0x39);
+  searcher->move();
+  searcher->setHeading(EAST);
+  searcher->move();
+  searcher->setHeading(SOUTH);
+  searcher->move();
+  searcher->setHeading(WEST);
+  searcher->move();
+  searcher->setHeading(NORTH);
+  EXPECT_EQ(0x39, searcher->location());
+  EXPECT_EQ(NORTH,searcher->heading());
 }
 
 
@@ -87,10 +116,10 @@ TEST_F (SearcherTest, moveWithMaze_smallCircuitReturnToStart) {
  * direction array and stop at the appropriate mLocation
  */
 TEST_F (SearcherTest, MouseRunTo_EmptyMaze_StartToGoal_MouseAtGoal) {
-  barney->setMazeWalls(emptyMaze, 256);
-  int steps = barney->runTo(0x77);
+  searcher->setMapFromFileData(emptyMaze, 256);
+  int steps = searcher->runTo(0x77);
   EXPECT_EQ(14,steps);
-  EXPECT_EQ(0x77,barney->location()) << "{" << barney->location() << ", " << +barney->heading() << "}";
+  EXPECT_EQ(0x77,searcher->location()) << "{" << searcher->location() << ", " << +searcher->heading() << "}";
 }
 
 /*
@@ -98,16 +127,16 @@ TEST_F (SearcherTest, MouseRunTo_EmptyMaze_StartToGoal_MouseAtGoal) {
  * direction array and stop at the appropriate mLocation
  */
 TEST_F (SearcherTest, MouseRunTo_EmptyMaze_ToGoalFromAnywhere) {
-  barney->setMazeWalls(japan2007, 256);
+  searcher->setMapFromFileData(japan2007, 256);
   maze->copyMazeFromFileData(japan2007,256);
   maze->flood(0x77);
-  int steps = barney->runTo(0x77);
+  int steps = searcher->runTo(0x77);
   EXPECT_EQ(72,steps);
   for(uint16_t loc = 0; loc < maze->numCells(); loc++) {
-    barney->setLocation(loc);
-    barney->setHeading(WEST);
-    barney->runTo(0x77);
-    EXPECT_EQ(0x77, barney->location()) << "{" << barney->location() << ", " << +barney->heading() << "}";
+    searcher->setLocation(loc);
+    searcher->setHeading(WEST);
+    searcher->runTo(0x77);
+    EXPECT_EQ(0x77, searcher->location()) << "{" << searcher->location() << ", " << +searcher->heading() << "}";
   }
 }
 
@@ -116,17 +145,17 @@ TEST_F (SearcherTest, MouseRunTo_EmptyMaze_ToGoalFromAnywhere) {
  * direction array and stop at the appropriate mLocation
  */
 TEST_F (SearcherTest, MouseRunTo_EmptyMaze_ToStartFromAnywhere) {
-  barney->setMazeWalls(japan2007, 256);
+  searcher->setMapFromFileData(japan2007, 256);
   maze->copyMazeFromFileData(japan2007,256);
   maze->flood(maze->home());
-  barney->setLocation(0x01);
-  int steps = barney->runTo(maze->home());
+  searcher->setLocation(0x01);
+  int steps = searcher->runTo(maze->home());
   EXPECT_EQ(1,steps);
   for(uint16_t loc = 1; loc < maze->numCells(); loc++) {
-    barney->setLocation(loc);
-    barney->setHeading(WEST);
-    int steps = barney->runTo(maze->home());
-    EXPECT_EQ(maze->home(), barney->location()) << "{" << barney->location() << ", " << +barney->heading() << "}";
+    searcher->setLocation(loc);
+    searcher->setHeading(WEST);
+    int steps = searcher->runTo(maze->home());
+    EXPECT_EQ(maze->home(), searcher->location()) << "{" << searcher->location() << ", " << +searcher->heading() << "}";
     EXPECT_GT(steps,0);
   }
 }
@@ -136,9 +165,9 @@ TEST_F (SearcherTest, MouseRunTo_EmptyMaze_ToStartFromAnywhere) {
  * Taiwan2015 maze has closed in cells at 0x5C and 0x5D
  */
 TEST_F (SearcherTest, MouseRunTo_RunToGoal_StartClosedIn_Error) {
-  barney->setMazeWalls(taiwan2015, 256);
-  barney->setLocation(0x5D);
-  int steps = barney->runTo(maze->goal());
+  searcher->setMapFromFileData(taiwan2015, 256);
+  searcher->setLocation(0x5D);
+  int steps = searcher->runTo(maze->goal());
   EXPECT_EQ(int(MazeSearcher::E_NO_ROUTE),steps);
 }
 
@@ -147,8 +176,8 @@ TEST_F (SearcherTest, MouseRunTo_RunToGoal_StartClosedIn_Error) {
  * Taiwan2015 maze has closed in cells at 0x5C and 0x5D
  */
 TEST_F (SearcherTest, MouseRunTo_RunToGoal_TargetClosedIn_Error) {
-  barney->setMazeWalls(taiwan2015, 256);
-  int steps = barney->runTo(0x5C);
+  searcher->setMapFromFileData(taiwan2015, 256);
+  int steps = searcher->runTo(0x5C);
   EXPECT_EQ(int(MazeSearcher::E_NO_ROUTE),steps);
 }
 
@@ -170,9 +199,10 @@ TEST_F (SearcherTest, MouseRunTo_RunToGoal_TargetClosedIn_Error) {
 TEST_F (SearcherTest, MouseSearchTo_EmptyMaze_Success) {
   Maze * testMaze = new Maze(16);
   testMaze->setWall(0x07,EAST);
-  barney->setRealMaze(testMaze);
-  maze->setFloodType(Maze::MANHATTAN_FLOOD);
-  int steps = barney->searchTo(0x77);
+  testMaze->setFloodType(Maze::MANHATTAN_FLOOD);
+  searcher->setRealMaze(testMaze);
+
+  int steps = searcher->searchTo(0x77);
   EXPECT_EQ(16,steps);
 }
 
@@ -182,10 +212,10 @@ TEST_F (SearcherTest, MouseSearchTo_EmptyMaze_Success) {
  */
 TEST_F (SearcherTest, MouseRunTo_SearchToTarget_VerboseMode) {
   Maze * testMaze = new Maze(16);
-  barney->setRealMaze(testMaze);
+  searcher->setRealMaze(testMaze);
   //barney->setVerbose(true);
-  barney->maze()->setFloodType(Maze::MANHATTAN_FLOOD);
-  int steps = barney->searchTo(0x7);
+  searcher->map()->setFloodType(Maze::MANHATTAN_FLOOD);
+  int steps = searcher->searchTo(0x7);
   EXPECT_EQ(7,steps);
 }
 
@@ -194,9 +224,10 @@ TEST_F (SearcherTest, MouseRunTo_SearchToTarget_VerboseMode) {
  *
  */
 TEST_F (SearcherTest, MouseRunTo_SearchToTarget_ManhattanFlood) {
-  barney->setRealMaze(maze);
-  barney->maze()->setFloodType(Maze::MANHATTAN_FLOOD);
-  int steps = barney->searchTo(0x77);
+  maze->copyMazeFromFileData(japan2007,256);
+  searcher->setRealMaze(maze);
+  searcher->map()->setFloodType(Maze::MANHATTAN_FLOOD);
+  int steps = searcher->searchTo(0x77);
   EXPECT_EQ(130,steps);
 //  MazePrinter::printVisitedDirs(barney->maze());
 }
@@ -206,11 +237,13 @@ TEST_F (SearcherTest, MouseRunTo_SearchToTarget_ManhattanFlood) {
  *
  */
 TEST_F (SearcherTest, MouseSearchToTarget_RunLengthFlood) {
-  barney->setRealMaze(maze);
-  barney->maze()->setFloodType(Maze::RUNLENGTH_FLOOD);
-  int steps = barney->searchTo(0x77);
+  maze->copyMazeFromFileData(japan2007,256);
+  searcher->setRealMaze(maze);
+  searcher->setSearchMethod(MazeSearcher::SEARCH_NORMAL);
+  searcher->map()->setFloodType(Maze::RUNLENGTH_FLOOD);
+  int steps = searcher->searchTo(0x77);
   EXPECT_EQ(142,steps);
-//  MazePrinter::printVisitedDirs(barney->maze());
+
 }
 
 
@@ -219,11 +252,10 @@ TEST_F (SearcherTest, MouseSearchToTarget_RunLengthFlood) {
  */
 TEST_F (SearcherTest, MouseSearchToTarget_LeftWall_Fail) {
   maze->copyMazeFromFileData(emptyMaze,256);
-  barney->setVerbose(false);
-  barney->setRealMaze(maze);
-  barney->maze()->setFloodType(Maze::RUNLENGTH_FLOOD);
-  barney->setSearchMethod(MazeSearcher::SEARCH_LEFT_WALL);
-  int steps = barney->searchTo(0x77);
+  searcher->setRealMaze(maze);
+  searcher->map()->setFloodType(Maze::RUNLENGTH_FLOOD);
+  searcher->setSearchMethod(MazeSearcher::SEARCH_LEFT_WALL);
+  int steps = searcher->searchTo(0x77);
   EXPECT_EQ(MazeSearcher::E_ROUTE_TOO_LONG,steps);
 }
 
@@ -232,10 +264,10 @@ TEST_F (SearcherTest, MouseSearchToTarget_LeftWall_Fail) {
  */
 TEST_F (SearcherTest, MouseSearchToTarget_LeftWall_Succeed) {
   maze->copyMazeFromFileData(japan2007,256);
-  barney->setRealMaze(maze);
-  barney->maze()->setFloodType(Maze::RUNLENGTH_FLOOD);
-  barney->setSearchMethod(MazeSearcher::SEARCH_LEFT_WALL);
-  int steps = barney->searchTo(0x77);
+  searcher->setRealMaze(maze);
+  searcher->map()->setFloodType(Maze::RUNLENGTH_FLOOD);
+  searcher->setSearchMethod(MazeSearcher::SEARCH_LEFT_WALL);
+  int steps = searcher->searchTo(0x77);
   EXPECT_EQ(84,steps);
 }
 
@@ -246,24 +278,19 @@ TEST_F (SearcherTest, MouseSearchToTarget_LeftWall_Succeed) {
  *
  */
 TEST_F (SearcherTest, MouseRunTo_SearchOutAndIn_RunLengthFlood) {
-  barney->setRealMaze(maze);
-  maze->copyMazeFromFileData(japan2007,256);
+  searcher->map()->setFloodType(Maze::RUNLENGTH_FLOOD);
   int steps = 0;
-  std::cout << "\n\nRunlength pass 0: ";
-  barney->maze()->testForSolution();
-  std::cout << barney->maze()->costDifference() << "  " << steps << " steps" << std::endl;
-  barney->maze()->setFloodType(Maze::RUNLENGTH_FLOOD);
-  steps += barney->searchTo(0x77);
-  steps += barney->searchTo(0x00);
+  steps += searcher->searchTo(0x77);
+  steps += searcher->searchTo(0x00);
   EXPECT_EQ(228,steps) << "The solution is not always correct. This is not right";
-  barney->maze()->testForSolution();
-  std::cout << "Runlength pass 1: ";
-  std::cout << barney->maze()->costDifference() << "  " << steps << " steps" << std::endl;
-  steps += barney->searchTo(0x77);
-  steps += barney->searchTo(0x00);
-  barney->maze()->testForSolution();
-  std::cout << "Runlength pass 2: ";
-  std::cout << barney->maze()->costDifference() << "  " << steps << " steps" << std::endl;
+  steps = 0;
+  steps += searcher->searchTo(0x77);
+  steps += searcher->searchTo(0x00);
+  EXPECT_EQ(150,steps) << "The solution is not always correct. This is not right";
+  steps = searcher->searchTo(0x77);
+  EXPECT_EQ(72,steps) << "The solution is not always correct. This is not right";
+  steps = searcher->searchTo(0x00);
+  EXPECT_EQ(72,steps) << "The solution is not always correct. This is not right";
 }
 
 
@@ -271,26 +298,20 @@ TEST_F (SearcherTest, MouseRunTo_SearchOutAndIn_RunLengthFlood) {
  *
  */
 TEST_F (SearcherTest, MouseRunTo_SearchOutAndIn_ManhattanFlood) {
-  barney->setRealMaze(maze);
-  barney->maze()->setFloodType(Maze::MANHATTAN_FLOOD);
+  searcher->map()->setFloodType(Maze::MANHATTAN_FLOOD);
   int steps=0;
-  std::cout << "\n\nManhattan pass 0: ";
-  barney->maze()->testForSolution();
-  std::cout << barney->maze()->costDifference() << "  " << steps << " steps" << std::endl;
-  steps += barney->searchTo(0x77);
-  steps += barney->searchTo(0x00);
+  steps += searcher->searchTo(0x77);
+  steps += searcher->searchTo(0x00);
   EXPECT_EQ(208,steps) << "The solution is not always correct. This is not right";
-  //MazePrinter::printVisitedDirs(barney->maze());
-  barney->maze()->testForSolution();
-  std::cout << "Manhattan pass 1: ";
-  std::cout << barney->maze()->costDifference() << "  " << steps << " steps" << std::endl;
-  steps += barney->searchTo(0x77);
-  steps += barney->searchTo(0x00);
-  barney->maze()->testForSolution();
-  std::cout << "Manhattan pass 2: ";
-  std::cout << barney->maze()->costDifference() << "  " << steps << " steps" << std::endl;
+  steps = 0;
+  steps += searcher->searchTo(0x77);
+  steps += searcher->searchTo(0x00);
+  EXPECT_EQ(164,steps) << "The solution is not always correct. This is not right";
+  steps = searcher->searchTo(0x77);
+  EXPECT_EQ(72,steps) << "The solution is not always correct. This is not right";
+  steps = searcher->searchTo(0x00);
+  EXPECT_EQ(72,steps) << "The solution is not always correct. This is not right";
 }
-
 
 
 
