9dd07dfca64e7ce2802a87e820665acc5b772856
Peter Harrison <peter.harrison@helicron.net>
2017-06-28 00:06:50 +0100

    tidy the mazedata.h header

--- maze/mazedata.h
+++ maze/mazedata.h
@@ -9,17 +9,17 @@
  */
 
 #ifndef MAZEDATA_H
-#define    MAZEDATA_H
+#define MAZEDATA_H
 
 #include <stdint.h>
 
-extern const uint8_t testMaze5x5[]; // small pathfinder test maze
-extern const uint8_t testMaze1[]; // simple single short route
-extern const uint8_t testmaze2[]; // two routes - favours runlength flood
+extern const uint8_t testMaze5x5[];    // small pathfinder test maze
+extern const uint8_t testMaze1[];      // simple single short route
+extern const uint8_t testmaze2[];      // two routes - favours runlength flood
 extern const uint8_t taiwan2015[];
 extern const uint8_t practice_maze[];
 extern const uint8_t emptyMaze[];
-extern const uint8_t japan2007[];
+extern const uint8_t japan2007[];      // has a wall-following solution!
 extern const uint8_t diag_test_maz[];
 extern const uint8_t test1_maz[];
 extern const uint8_t japan_2014_heat[];
