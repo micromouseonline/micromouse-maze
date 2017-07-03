1a1dafcdea26ec79b431aaa03a17fcff16608c44
Peter Harrison <peter.harrison@helicron.net>
2017-07-03 23:34:12 +0100

    test all the mazes for likley one-pass goodness

--- main.cpp
+++ main.cpp
@@ -16,7 +16,6 @@
 #include <stdlib.h>
 #include "maze.h"
 #include "mazeprinter.h"
-//#include "mazepathfinder.h"
 #include "mazesearcher.h"
 #include <glob.h>
 
@@ -24,8 +23,6 @@ uint8_t wallData[1024];
 
 int ReadRealWallsFromFile(char *filename) {
   FILE *fp;
-
-//  ClearWallData(); /* in case we do not find a file */
   if ((fp = fopen(filename, "rb")) == NULL) {
     return 1;
   }
@@ -33,21 +30,26 @@ int ReadRealWallsFromFile(char *filename) {
     fclose(fp);
     return 2;
   };
-
-//  fseek(fp, 0L, SEEK_END);
-//  long size = ftell(fp);
-//  // a standard maze uses 256 bytes
-//  // assume the first 256 bytes are the wall data
-//  if (size >= 256) {
-//    rewind(fp);
-//    for (loc.col = 0; loc.col < MAZE_COLS; loc.col++) {
-//      for (loc.row = 0; loc.row < MAZE_ROWS; loc.row++) {
-//        wallData[loc.row][loc.col] = fgetc(fp);// & ~VISITED;
-//      }
-//    }
-//  }
   fclose(fp);
   return 0;
+}
+
+static char *strpad(const char *string, char pad, size_t fieldSize);
+
+char *strpad(const char *string, char pad, size_t fieldSize) {
+  size_t ssize = strlen(string);
+  size_t padSize = fieldSize - ssize;
+  assert(padSize > 0);
+  char padding[64];
+  memset(padding, pad, 62);
+  padding[63] = 0;
+  char *padded = (char *) malloc(fieldSize + 1);
+  strncpy(padded, string, fieldSize);
+  strncat(padded, padding, padSize);
+//  memset(padded, pad, fieldSize);
+  padded[fieldSize] = 0;
+//  strcpy(padded , string);
+  return padded;
 }
 
 /*
@@ -62,12 +64,11 @@ int main(int argc, char **argv) {
     int onePassCount = 0;
     int chancerCount = 0;
     printf("   FULL  MODOPEN   MODALL\n");
-    barney.setSearchMethod(MazeSearcher::SEARCH_LEFT_WALL);
+    maze.setFloodType(Maze::RUNLENGTH_FLOOD);
+    barney.setSearchMethod(MazeSearcher::SEARCH_NORMAL);
     for (unsigned int i = 0; i < glob_result.gl_pathc; ++i) {
-     std::cout << "\n\n\n";
-      std::cout << glob_result.gl_pathv[i] << " : ";
+      std::cout << "";
       ReadRealWallsFromFile(glob_result.gl_pathv[i]);
-//      maze.resetToEmptyMaze();
       maze.load(wallData);
       maze.copyMazeFromFileData(wallData, 256);
       maze.flood(0x77);
@@ -75,77 +76,40 @@ int main(int argc, char **argv) {
       barney.map()->resetToEmptyMaze();
       barney.setLocation(0x00);
       int steps = barney.searchTo(0x77);
-      //steps += barney.searchTo(0x00);
-      std::cout << steps << " steps   ";
+      steps += barney.searchTo(0x00);
       barney.map()->testForSolution();
       int costDifference = barney.map()->costDifference();
       int openCost = barney.map()->openMazeCost();
-      int residual = (100*costDifference)/openCost;
-      std::cout << costDifference << " [" << residual<< "] ";
+      int residual = (100 * costDifference) / openCost;
 
-      if (costDifference == 0){
-        onePassCount ++;
-        std::cout << "             <-----";
+      if (residual < 5) {
+//        MazePrinter::printVisitedDirs(barney.map());
+        char *name = strpad(glob_result.gl_pathv[i], ' ', 43);
+        std::cout << name;
+        delete name;
+        std::cout.width(6);
+        std::cout << std::right;
+        std::cout << steps << " steps";
+        std::cout << " delta " << costDifference << " (" << residual << "%)";
+        if (costDifference == 0) {
+        onePassCount++;
+            std::cout << " <---- ";
+        std::cout << std::endl;
+        } else if (residual < 5 && residual >= 0) {
+            chancerCount++;
+            std::cout << " ***** ";
+        std::cout << std::endl;
+          }
+        }
       }
-      if (residual < 5 && residual >= 0){
-        chancerCount++;
-        std::cout << " ** ";
-      }
-      std::cout << std::endl;
-      MazePrinter::printVisitedDirs(barney.map());
 
-//    for (int i = 1; i < argc; i++) {
-//      ReadRealWallsFromFile(argv[i]);
-//
-//      MouseInit();
-//      MazeInit();
-//      int stepsFull = MouseSearchToFullFlood(target);
-//      MouseInit();
-//      MazeInit();
-//      int stepsModOpen = MouseSearchToModifiedFlood(target);
-//      MouseInit();
-//      MazeInit();
-//      int stepsModAll = MouseSearchToModifiedFloodAll(target);
-//      char x, y, z;
-//      x = ' ';
-//      y = ' ';
-//      z = ' ';
-//      if ((stepsFull < stepsModOpen) && (stepsFull < stepsModAll)) {
-//        x = '*';
-//        fullCount++;
-//      }
-//      if ((stepsModOpen < stepsFull) && (stepsModOpen < stepsModAll)) {
-//        y = '*';
-//        modOpenCount++;
-//      }
-//      if ((stepsModAll < stepsFull) && (stepsModAll < stepsModOpen)) {
-//        z = '*';
-//        modAllCount++;
-//      }
-//      if ((stepsFull >= stepsModAll) || (stepsFull >= stepsModOpen)) {
-//        x = '!';
-//        fullfullCount++;
-//      }
-//      totalMazes++;
-//      printf("%7d%c  %9d%c  %9d%c  steps for %s\n", stepsFull, x, stepsModOpen, y, stepsModAll, z, argv[i]);
-//      //printf ("%4d : %3d %c %3d steps for %s\n", stepsFull - stepsModAll, stepsFull, (stepsFull > stepsModAll) ? '>' : '<', stepsModAll,  argv[i]);
-    }
 
     printf("\n%d mazes:\n", glob_result.gl_pathc);
     printf(" one Pass mazes: %3d\n", onePassCount);
     printf(" Possible chancer  mazes: %3d\n", chancerCount);
 
   } else {
-//    char fileName[] = "mazefiles/minos03f.maz";
-//    ReadRealWallsFromFile(fileName);
-//    MouseInit();
-//    MazeInit();
-//    printf(" : %s\n", fileName);
-//    int steps = MouseSearchToFullFlood(target);
-//    printf("searched in %d steps\n", steps);
-//    int length = IsolatePath(Home(), Goal());
-//    printf("Path length is %d cells\n", length);
-
+    printf(" you should provide the name of at least one maze file on the command line\n");
   }
   return (EXIT_SUCCESS);
 }
