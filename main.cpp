677d81624b3bc41c6e4746e3c229bc62939289be
Peter Harrison <peter.harrison@helicron.net>
2018-09-04 22:44:09 +0100

    remove references to the Searcher from main()

--- main.cpp
+++ main.cpp
@@ -17,7 +17,7 @@
 #include <stdlib.h>
 #include "maze.h"
 #include "mazeprinter.h"
-#include "mazesearcher.h"
+
 #include <glob.h>
 #include <libgen.h>
 
@@ -63,70 +63,23 @@ char *strpad(const char *string, char pad, size_t fieldSize) {
  *
  */
 int main(int argc, char **argv) {
-  MazeSearcher barney;
+
   glob_t glob_result;
   glob(argv[1], GLOB_TILDE, nullptr, &glob_result);
   std::cout << "Processing " << glob_result.gl_pathc << " mazes" << std::endl;
 
   if (argc >= 1) {
     Maze maze(16);
-    int onePassCount = 0;
-    int chancerCount = 0;
     maze.setFloodType(Maze::RUNLENGTH_FLOOD);
     for (unsigned int i = 0; i < glob_result.gl_pathc; ++i) {
       std::cout << "";
-      if (0 !=  ReadRealWallsFromFile(glob_result.gl_pathv[i])) {
+      if (0 != ReadRealWallsFromFile(glob_result.gl_pathv[i])) {
         continue;
       };
       maze.load(wallData);
       maze.copyMazeFromFileData(wallData, 256);
       maze.flood(0x77);
-      barney.setRealMaze(&maze);
-      barney.map()->resetToEmptyMaze();
-      barney.setLocation(0x00);
-      //Run a search out and back again, recording the total steps needed
-      int steps = barney.searchTo(0x77);
-      steps += barney.searchTo(0x00);
-      barney.map()->testForSolution();
-      int costDifference = barney.map()->costDifference();
-      int openCost = barney.map()->openMazeCost();
-      // how close to a complete solution are we?
-      int residual = (100 * costDifference) / openCost;
-
-      if (residual > 5) {
-        //        std::cout << glob_result.gl_pathv[i] << std::endl;
-        MazePrinter::printVisitedDirs(barney.map());
-
-      }
-      char *fileName = basename(glob_result.gl_pathv[i]);
-      char *name = strpad(fileName, ' ', 38);
-      std::cout << name;
-      //      delete name;
-      std::cout.width(6);
-      std::cout << std::right;
-      std::cout << steps << " steps - delta ";
-      std::cout.width(3);
-      std::cout << std::right;
-      std::cout << costDifference << " (";
-      std::cout.width(3);
-      std::cout << std::right;
-      std::cout << residual << "%)";
-      if (costDifference == 0) {
-        onePassCount++;
-        std::cout << " Fully solved";
-      } else if (residual < 5 && residual >= 0) {
-        chancerCount++;
-        std::cout << " Worth a run ";
-      }
-      std::cout << std::endl;
-      delete name;
-
     }
-
-    printf("\n%d mazes:\n", (int)glob_result.gl_pathc);
-    printf(" one Pass mazes: %3d\n", onePassCount);
-    printf(" Possible chancer  mazes: %3d\n", chancerCount);
-
   } else {
     printf(" you should provide the name of at least one libMaze file on the command line\n");
   }
